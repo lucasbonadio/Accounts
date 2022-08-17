a551e5e40d655389fbca4b1c0817afc05f0f6c0c
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-08-17 05:24:52 -0300

    Function to create an account

--- index.mjs
+++ index.mjs
@@ -14,14 +14,56 @@ function operation() {
         message: 'What do you want to do?',
         choices: ['Make account', 'Check balance', 'Deposit', 'Withdraw', 'Exit'],
     },
-]).then(response => {
-   const action = response['action'];
+    ]).then(response => {
+        const action = response['action'];
 
-   console.log(action)
-    
-}).catch(err => {
-    console.log(err)
-})
+        if (action === 'Make account') {
+            createAccount()
+        }
+
+    }).catch(err => {
+        console.log(err)
+    })
 }
+
+// Functions associated to create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Thanks for choosing our bank!'));
+    console.log(chalk.green('Define the options of your account in the next step'));
+    buildAccount();
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Enter a name for your account:'
+        }
+    ]).then(response => {
+        const accountName = response['accountName'];
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.red('This account already exist, choose other name'))
+            buildAccount();
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', (error) => {
+            console.log(error);
+        })
+
+        console.log(chalk.green("Congratulations, your account was created with a sucessful"));
+        operation();
+
+    }).catch(err => console.log(err))
+}
+
+
+
+
 
 
