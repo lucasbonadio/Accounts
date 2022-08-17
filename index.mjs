94ae44053bdb61b59d1cd8aa8b2387f6d581cf61
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-08-17 06:27:14 -0300

    Function to add an amount to account

--- index.mjs
+++ index.mjs
@@ -19,13 +19,13 @@ function operation() {
 
         if (action === 'Make account') {
             createAccount()
-        } else if(action === 'Check balance') {
+        } else if (action === 'Check balance') {
 
-        } else if(action === 'Deposit') {
+        } else if (action === 'Deposit') {
+            deposit()
+        } else if (action === 'Withdraw') {
 
-        } else if(action === 'Withdraw') {
-
-        } else if(action === 'Exit') {
+        } else if (action === 'Exit') {
             console.log(chalk.bgCyan("Thanks for using the Accounts!"))
             process.exit();
         }
@@ -52,11 +52,11 @@ function buildAccount() {
         const accountName = response['accountName'];
 
         if (!fs.existsSync('accounts')) {
-            fs.mkdirSync('accounts')
+            fs.mkdirSync('accounts');
         }
 
         if (fs.existsSync(`accounts/${accountName}.json`)) {
-            console.log(chalk.red('This account already exist, choose other name'))
+            console.log(chalk.red('This account already exists, choose other name'))
             buildAccount();
             return
         }
@@ -68,10 +68,68 @@ function buildAccount() {
         console.log(chalk.green("Congratulations, your account was created with a sucessful"));
         operation();
 
-    }).catch(err => console.log(err))
+    }).catch(err => console.log(err));
 }
 
+// Function to Deposit
+function deposit(accountName) {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Enter the name of account to deposit:'
+    }]).then(response => {
+        const accountName = response['accountName']
 
+        if (!checkAccount(accountName)) {
+            return deposit();
+        }
+
+        inquirer.prompt([{
+            name: 'amount',
+            message: 'How much do you want to deposit:'
+        }]).then(response => {
+            const amount = response['amount'];
+            addAmount(accountName, amount);
+            operation();
+        }).catch(err => console.log(err));
+
+
+    }).catch(err => console.log(err));
+
+}
+
+//Functions that may be reused
+function checkAccount(accountName) {
+    if (!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.red("This account doesn't exists, please enter other name"));
+        return false;
+    } else {
+        return true
+    }
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName);
+
+    if (!amount) {
+        console.log(chalk.red('Put a value to deposit'));
+        deposit();
+    } else {
+        accountData.balance += Number(amount);
+        fs.writeFileSync(`accounts/${accountName}.json`, JSON.stringify(accountData), (err) => console.log(err));
+    }
+
+    console.log(chalk.green(`Was deposited with sucessful the amount of the ${amount}`));
+}
+
+function getAccount(accountName) {
+    const accountJson = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf-8',
+        flag: 'r'
+    })
+
+    return JSON.parse(accountJson);
+
+}
 
 
 
